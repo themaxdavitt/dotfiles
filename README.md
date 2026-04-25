58f76188ad1054865e8bf410586a658c5cab79cc
Max Davitt <max@davitt.me>
2026-04-25 13:09:27 -0400

    Re-do everything

---
+++ README.md
@@ -0,0 +1,7 @@
+Powered by [_chezmoi_](https://chezmoi.io).
+
+---
+
+TODO: Document everything else.
+
+<!-- TODO: mention configuring OpenRouter to only use good providers + ZDR -->
--- setup.sh
+++ setup.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
+set -e
 
-# Make Bash a little easier to work with
-# https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425
-set -euo pipefail
+echo "TODO: Implement :)"
 
-# Change to script directory
-# https://stackoverflow.com/a/3355423
-cd "$(dirname "$0")"
-
-# TODO: Set up a bunch of other stuff before this
-
-# Install Stow, a symlink manager
-brew install stow
-
-# Create symlinks in home directory
-stow
+# mise install --locked "$@"
+# compaudit | xargs chmod g-w
